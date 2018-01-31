01a826e9fed5e2f46abd58e887da2ac184610a95
Nicholas Iloba <nick_iloba@hotmail.com>
2018-01-31 00:44:43 -0500

    moved the needle on node.js

--- Delicious/controllers/storeController.js
+++ Delicious/controllers/storeController.js
@@ -4,5 +4,13 @@ exports.myMiddleware = (req, res, next) => {
 }
 
 exports.homePage = (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'index'});
 };
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {title: 'Add store'})
+};
+
+exports.createStore = (req, res) => {
+    res.json(req.body);
+}
