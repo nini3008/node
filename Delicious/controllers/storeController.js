66e461ffd36bfcccacc4e27f5a215db0a90f5caf
Nicholas Iloba <nick_iloba@hotmail.com>
2018-02-14 21:25:29 -0500

    node lesson 14

--- Delicious/controllers/storeController.js
+++ Delicious/controllers/storeController.js
@@ -14,3 +14,23 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review? `)
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    // Query database for all stores using the find function
+     const stores = await Store.find();
+     res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({ _id: req.params.id });
+    res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true
+    }).exec();
+    req.flash('success', `Successfully Updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+};
